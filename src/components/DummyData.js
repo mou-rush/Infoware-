844ba75aa4256bac10e0cadc9f46017f76a6b018
Moureen <moureenham@gmail.com>
2021-11-27 17:08:06 +0530

    mock data created

---
+++ src/components/DummyData.js
@@ -0,0 +1,60 @@
+const data = {
+  productItems: [
+    {
+      id: "1",
+      name: "Vivo V20",
+      price: 23333,
+      image: "./assets/vivo",
+    },
+    {
+      id: "2",
+      name: "Boat Headphones",
+      price: 999,
+      image: "./assets/boad-headphones.jpg",
+    },
+    {
+      id: "3",
+      name: "Sun Glasses",
+      price: 890,
+      image: "./assets/sunglasses.jpg",
+    },
+    {
+      id: "4",
+      name: "F&D Speaker",
+      price: 950,
+      image: "./assets/speaker.jpg",
+    },
+    {
+      id: "5",
+      name: "Hp Keyboard",
+      price: 500,
+      image: "./assets/hp-keyboard.jpg",
+    },
+    {
+      id: "6",
+      name: "Logi Mouse",
+      price: 799,
+      image: "./assets/logi-mouse.jpg",
+    },
+    {
+      id: "7",
+      name: "SanDisk Flash Drive",
+      price: 360,
+      image: "./assets/flash-drive.jpg",
+    },
+    {
+      id: "8",
+      name: "Sumsung Charger",
+      price: 2690,
+      image: "./assets/sumsung-charger.jpg",
+    },
+    {
+      id: "9",
+      name: "Iphone XR",
+      price: 50333,
+      image: "./assets/iphone.jpg",
+    },
+  ],
+};
+
+export default data;
